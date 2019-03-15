71a2a202535ada13630ca5e16df5d8fa6b6fbcbe
Annie Martens <anniemartens@Annies-MacBook-Pro.local>
2019-03-15 13:58:22 -0500

    testing format of oauth creat

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,7 +14,17 @@ class SessionsController < ApplicationController
   end
 
   def create_oath
-    puts request.env['omniauth.auth'][:info][:email]
+    user_email = request.env['omniauth.auth'][:info][:email]
+    user = User.find_by(email: user_email)
+    if user?
+      log_in user
+    else
+      @user = User.new(email: user_email, name: request.env['omniauth.auth'][:info][:name])
+      if user.save? 
+        redirect_to user
+      else
+        render 'new'
+      end
   end
 
   def destroy
